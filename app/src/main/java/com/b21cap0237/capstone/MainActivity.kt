eabc7479ce9ac6337b7be8fc49caab37daabab72
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-05-09 12:37:47 +0700

    asset commit

---
+++ app/src/main/java/com/b21cap0237/capstone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.b21cap0237.capstone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
